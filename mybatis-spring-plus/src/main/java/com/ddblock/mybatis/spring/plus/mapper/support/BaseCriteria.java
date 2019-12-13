fbbf680f933b74f9f195d7c1b3dd33741368d182
XJ2017 <504283451@qq.com>
2019-12-13 17:23:53 +0800

    修复BaseCriterion中漏掉的ForJDBCDate

--- mybatis-spring-plus/src/main/java/com/ddblock/mybatis/spring/plus/mapper/support/BaseCriteria.java
+++ mybatis-spring-plus/src/main/java/com/ddblock/mybatis/spring/plus/mapper/support/BaseCriteria.java
@@ -58,4 +58,22 @@ public abstract class BaseCriteria {
         addCriterion(condition, new java.sql.Date(value.getTime()), property);
     }
 
+    protected void addCriterionForJDBCDate(String condition, Date value1, Date value2, String property) {
+        if (value1 == null || value2 == null) {
+            throw new RuntimeException("Between values for " + property + " cannot be null");
+        }
+        addCriterion(condition, new java.sql.Date(value1.getTime()), new java.sql.Date(value2.getTime()), property);
+    }
+
+    protected void addCriterionForJDBCDate(String condition, List<Date> values, String property) {
+        if (values == null || values.size() == 0) {
+            throw new RuntimeException("Value list for " + property + " cannot be null or empty");
+        }
+        List<java.sql.Date> dateList = new ArrayList<>();
+        for (Date value : values) {
+            dateList.add(new java.sql.Date(value.getTime()));
+        }
+        addCriterion(condition, dateList, property);
+    }
+
 }
